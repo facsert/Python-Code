e358c7e7209265e3ab204c1fb7d570217c546061
facser <facsert@outlook.com>
2023-08-05 22:59:22 +0800

    logger
    1.add logger module
    2.flatDict add commit.
    
    Signed-off-by: facser <facsert@outlook.com>

--- flatDict.py
+++ flatDict.py
@@ -1,14 +1,35 @@
+'''
+Author: facsert
+Date: 2023-08-03 21:41:52
+LastEditTime: 2023-08-05 21:05:17
+LastEditors: facsert
+Description: 
+'''
 
 from json import dumps
 
 class FlatDict(dict): 
     """扁平化字典"""   
-    def __init__(self, dict_depth):
-        super().__init__(dict_depth)
+
+    def __init__(self, *args, **kwargs):
+        '''
+        Description: 初始化属性, flat(扁平化字典) separator(分隔符)
+        Return: None
+        Attention: 对象存一个原生字典和扁平化字典
+        '''        
+        super().__init__(*args, **kwargs)
+        super().update(*args, **kwargs)
         self.flat = {}
         self.separator = '.'
 
     def update_dict(self, key, value):
+        '''
+        Description: 解析 key, 将多层 key 逐层解析写入原生字典
+        Param key str: 字典 key, 多层 key 包含分隔符 
+        Param value Any: 字典 value
+        Return: None
+        Attention: 
+        '''        
         dic = self
         keys = key.split(self.separator)
         for k in keys[:-1]:
@@ -21,6 +42,13 @@ class FlatDict(dict):
         self.flat_dict(self)
 
     def flat_dict(self, dic, parent_key=''):
+        '''
+        Description: 原生字典多层 key 通过分隔符连接写入 flat 字典
+        Param dic dict: 原生字典
+        Param parent_key dict: 父字典的 key 
+        Return: None
+        Attention: 任一层的字典 key value 都要保存
+        '''        
         for key, value in dic.items():
             new_key = f"{parent_key}{self.separator}{key}" if parent_key else key
             self.flat[new_key] = value
@@ -28,31 +56,77 @@ class FlatDict(dict):
                 self.flat_dict(value, new_key)
 
     def __setitem__(self, key, value):
+        '''
+        Description: 字典 [] 方式设置值
+        Param key str: 原生字典
+        Param value Any: 父字典的 key 
+        Return: None
+        Attention: 
+        '''        
         if self.separator in key:
             self.update_dict(key, value)
         else:
             super().__setitem__(key, value)
 
     def __getitem__(self, key):
+        '''
+        Description: 字典 [] 获取值
+        Param key str: 字典 key, 允许使用多层 key 
+        Return Any: 字典 key 对应的 value 
+        Attention: 
+        '''        
         try:
             return super().__getitem__(key)
         except KeyError:
             return self.flat[key]
 
     def __delitem__(self, key):
+        '''
+        Description: 字典删除 key-value
+        Param key str: 字典 key, 不允许使用多层 key 
+        Return: None
+        Attention: 只允许使用原生字典的 key
+        '''    
         super().__delitem__(key)
         self.flat = {}
         self.flat_dict(self)
 
     def __len__(self):
+        '''
+        Description: 获取原生字典长度
+        Return int: 字典长度 
+        Attention: 
+        '''        
         return super().__len__()
 
     def update(self, *args, **kwargs):
+        '''
+        Description: 更新字典
+        Return: None 
+        Attention: 用法与原生字典一致
+        '''        
         super().update(*args, **kwargs)
         self.flat = {}
         self.flat_dict(self)
 
     def __str__(self):
+        '''
+        Description: json 格式原生字典
+        Return srt: 字典字符串
+        Attention: 
+        '''        
         return dumps(self, indent=4)
 
+
+if __name__ == '__main__':
+    flat = FlatDict()
+    flat.update({
+        'a': {
+            'b': { 'c': 1 },
+        },
+        'e':3,    
+    })
+    print(flat['a.b'])
+    flat['a.c.d'] = 2
+    print(flat)
 
---
+++ logger.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*- 
+'''
+Author: facsert
+Date: 2023-08-05 21:06:59
+LastEditTime: 2023-08-05 22:57:18
+LastEditors: facsert
+Description: 
+'''
+
+from logging import (StreamHandler, FileHandler, Formatter, getLogger,
+    NOTSET, DEBUG, INFO, WARNING, ERROR, CRITICAL)
+
+# logging 显示等级定义
+# CRITICAL = 50
+# FATAL = CRITICAL
+# ERROR = 40
+# WARNING = 30
+# WARN = WARNING
+# INFO = 20
+# DEBUG = 10
+# NOTSET = 0
+
+class logger:
+    """格式化打印内容
+    
+    Example:
+        logger.info(str) 
+        logger.error(str)
+        
+    Args:
+        file:  打印内容重定向到的文件
+        level: 显示等级
+        format:打印格式
+    """
+    log = None
+    red = '\033[91m'
+    yellow = '\033[93m'
+    reset = '\033[0m'
+    file_format = "[%(levelname)-5s] %(asctime)s : %(message)s"
+    terminal_format = "[%(levelname)-5s] %(asctime)s : %(message)s"
+    
+    @classmethod
+    def create_logger(cls, file="report.log", level="info"):
+        '''
+        Description: 删除原有的 handler, 创建新的命令行和文本 handler
+        Param file str: log 输出文件
+        Param level str: 显示 log 的等级
+        Return: None
+        Attention: 多个 logger 对象会重复输出
+        '''    
+        cls.remove_handler()
+        cls.log = getLogger('log')
+        cls.level = cls.get_level(level)
+        cls.handle_terminal(cls.level, cls.terminal_format)
+        cls.handle_file(file, cls.level, cls.file_format)
+        cls.log.setLevel(cls.level)
+
+    @classmethod
+    def remove_handler(cls):
+        '''
+        Description: 遍历 logger 对象, 移除 handler
+        Return: None
+        Attention: 
+        '''        
+        if cls.log is None:
+            return
+        for handler in cls.log.handlers[:]:
+            handler.close()
+            cls.log.removeHandler(handler)
+        
+    @staticmethod
+    def get_level(level):
+        '''
+        Description: 根据等级字符串获取等级
+        Return int: 显示等级 
+        Attention: 若非法 level, 默认返回 INFO 
+        '''        
+        return {
+            'NOTSET': NOTSET,
+            'DEBUG': DEBUG,
+            'INFO': INFO,
+            'WARNING': WARNING,
+            'ERROR': ERROR,
+            'CRITICAL': CRITICAL,    
+        }.get(level.upper(), INFO)
+        
+    @classmethod
+    def handle_terminal(cls, level, format):
+        '''
+        Description: 配置命令行打印控制器
+        Param level str: 命令行显示 log 等级 
+        Param format str: 命令行 log 打印前缀 
+        Attention: 默认与全部使用设置显示等级, 可自定义
+        '''        
+        handler = StreamHandler()
+        handler.setLevel(level)
+        handler.setFormatter(Formatter(format))
+        cls.log.addHandler(handler)
+        
+    @classmethod
+    def handle_file(cls, file, level, format):
+        '''
+        Description: 配置文件打印控制器
+        Param file str:  log 输出文件 
+        Param level str: 文件记录 log 等级 
+        Param format str: 文件 log 打印前缀 
+        Attention: 默认与全部使用设置显示等级, 可自定义
+        '''  
+        handler = FileHandler(file)
+        handler.setLevel(level)
+        handler.setFormatter(Formatter(format))
+        cls.log.addHandler(handler)
+        
+    @classmethod
+    def info(cls, msg):
+        '''
+        Description: 打印 info log 
+        Param msg str: 打印内容 
+        Return msg str: 打印内容 
+        Attention: 
+        '''    
+        if cls.log is None:
+            cls.create_logger()
+        cls.log.info(msg)
+        return msg
+    
+    @classmethod
+    def error(cls, msg):
+        '''
+        Description: 打印 error log, 命令行显示为红色
+        Param msg str: 打印内容 
+        Return msg str: 打印内容 
+        Attention: 
+        '''   
+        if cls.log is None:
+            cls.create_logger()
+        cls.log.error(f'{cls.red}{msg}{cls.reset}')
+        return msg
+    
+    @classmethod
+    def warning(cls, msg):
+        '''
+        Description: 打印 warning log, 命令行显示为黄色
+        Param msg str: 打印内容 
+        Return msg str: 打印内容 
+        Attention: 
+        '''   
+        if cls.log is None:
+            cls.create_logger()
+        cls.log.warning(f'{cls.yellow}{msg}{cls.reset}')
+        return msg
+    
+  
+if __name__ == '__main__':
+    logger.info( "info log")
+    logger.warning("warning log")
+    logger.create_logger("summary.log", "info")
+    logger.info( "inof log")
+    logger.error("error log")
+
