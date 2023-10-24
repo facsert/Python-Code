317826b06c3acd5f2f4e5059f8514b9362287002
d30047236 <dingwenlong4@huawei.com>
2023-10-24 14:26:21 +0800

    ssh
    modify shell
    add set_env_var
    add chdir
    add kill_proc
    
    Signed-off-by: d30047236 <dingwenlong4@huawei.com>

--- ssh.py
+++ ssh.py
@@ -1,8 +1,8 @@
 '''
 Author: facsert
 Date: 2023-08-23 20:34:57
-LastEditTime: 2023-09-18 20:24:18
-LastEditors: facsert
+LastEditTime : 2023-10-24 14:25:17
+LastEditors  : Please set LastEditors
 Description: 
 '''
 
@@ -39,6 +39,10 @@ class Client:
             timeout=self.timeout
         )
         return client
+    
+    def close(self):
+        self.channel.close()
+        self.client.close()
 
     def exec(self, command, timeout=5, view=True):
         """执行非交互式 linux 命令, 并实时打印
@@ -96,7 +100,6 @@ class Client:
             若等待响应开始计时(resp > 0), 执行时间超过 timeout + resp_timeout 才算超时
             若命令一直有输出(响应计时 resp == 0), 执行 timeout 时间后退出
         """
-        # print(command) if view else False
         self.channel.settimeout(timeout) 
         self.channel.sendall(command + '\n')
         resp_timeout = min(timeout, resp_timeout)
@@ -106,8 +109,10 @@ class Client:
             if self.channel.recv_ready():
                 data, buff = f"{buff}{self.channel.recv(65535).decode('utf-8', 'ignore')}", ""
                 lines, output = data.splitlines(True), f"{output}{data}"
-                resp, buff, lines = 0, lines[-1], ([], lines[:-1])[view]
-                [print(line, end="", flush=True) for line in lines]
+                buff, lines, resp = ("", lines, 0) if data.endswith('\n') else (lines[-1], lines[:-1], 0)
+                for line in lines:
+                    print(line.rstrip()) if view else False
+                    resp = resp_timeout if expect != "" and expect in line else resp
             else:
                 sleep(0.1)
                 resp += 0.1
@@ -124,6 +129,89 @@ class Client:
 
     def sftp(self):
         return self.sftp
+    
+    def set_env_var(self, variable, value, view=False):
+        """ 设置环境变量
+        Params:
+          variable str : linux 命令
+          value    str : 命令返回预期值
+
+        Return:
+          succ bool: 环境变量设置成功
+
+        Attention:
+            前后打印环境变量值不同则判定环境变量设置成功
+        """
+        cmd = f'export {variable}={value}; echo "${variable}, code: $?"'
+        succ, output = self.run(cmd, "code: 0", resp_timeout=1, view=view)
+
+        if succ:
+            print(f"set environmet {variable}:{value}")
+        else:
+            print(f"set environmet {variable} to {value} fail")
+        return succ
+    
+    def chdir(self, path):
+        """ 变更通道当前目录
+        Params:
+          dir str : client 端绝对路径
+
+        Return:
+          succ bool: 切换成功
+        """
+        cmd = f'cd {path}; echo $PWD, code: $?'
+        succ, output = self.run(cmd, "code: 0", resp_timeout=1, view=False)
+        if succ:
+            print(f"change dir to {path}")
+        else:
+            print(f"change dir to {path} fail")
+        return succ
+    
+    def kill_proc(self, key, view=False, retry=30):
+        """ 杀死进程
+        Params:
+          key   str: 进程关键字
+          retry int: 重试次数
+
+        Return:
+          succ bool: 进程成功关闭
+
+        Attention:
+            通过关键字查询不到对应进程判定进程关闭
+        """
+
+        check = f"ps -eaf | grep -E {key} | grep -v grep"
+        kill = check + "|awk '{print $2}' | xargs kill -9"
+        father_kill = check + "|awk '{print $3}' | xargs kill -9"
+        while retry:
+            proc_alive, _ = self.exec(check, timeout=10, view=view)
+            self.exec((kill, father_kill)[retry < 5], timeout=10, view=view)
+            if proc_alive:
+                sleep(0.1)
+                retry -= 1
+                continue
+            break
+        return retry > 0
+
+    def update_json(self, file, items):
+        """ 更新远端 json 文件
+        Params:
+          file  str : client 端 json 文件绝对地址
+          items dict: 更新键值对
+
+        Return:
+          succ bool: 文件更新成功
+        """
+        try:
+            with self.sftp.open(file, 'r') as f:
+                dic = load(f)
+            dic.update(items)
+            with self.sftp.open(file, 'w') as f:
+                f.write(dumps(dic, indent=4))
+        except Exception as e:
+            print(f"update {file} failed: {e}")
+        return True
+
         
 
 if __name__ == "__main__":
