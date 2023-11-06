48090589754f5130af4529b84622d72f6f3617ec
d30047236 <dingwenlong4@huawei.com>
2023-11-06 17:25:21 +0800

    fastapi
    1.add personal swagger-ui
    
    Signed-off-by: d30047236 <dingwenlong4@huawei.com>

--- fastapi.py
+++ fastapi.py
@@ -6,21 +6,48 @@ LastEditors: facsert
 Description: 
 """
 
+import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.core import CORSMiddleware
+from os.path import join, dirname
+from os import getcwd
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+
 
 app = FastAPI()
-
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://localhost:8000"
-]
+app.mount(                                       # 挂载静态文件, 路由 /static 和本地 swagger-ui 文件夹 对应
+    '/static',                                   # /static -> ./static/swagger-ui/dist
+    StaticFiles(directory=join(getcwd(), 'static', 'swagger-ui', 'dist')),
+    name="static"
+)
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+      CORSMiddleware,
+      allow_origins=["*"],  
+      allow_credentials=True,
+      allow_methods=["*"],  
+      allow_headers=["*"],  
 )
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+    )
+
+@app.get("/")
+async def root():
+    return "root page"
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
