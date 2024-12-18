7bfa692b066e9cd386ea975757a6778de10d1d3c
facsert <facsert@outlook.com>
2024-12-18 22:14:48 +0800

    python
    1.websocket add loop and break
    
    Signed-off-by: facsert <facsert@outlook.com>

--- fastapi/api/v1/cmd.py
+++ fastapi/api/v1/cmd.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime
 
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
@@ -14,9 +15,24 @@ router = APIRouter()
 @router.websocket("/ws")
 async def run_cmd(websocket: WebSocket):
     await websocket.accept()
+    
+    first, task = True, None
+    async def send():
+        while True:
+            logger.info(f"log timestamp: {datetime.now()}")
+            await websocket.send_text(f"timestamp: {datetime.now()}")
+            await asyncio.sleep(2)
+
     while True:
         data = await websocket.receive_text()
+        if 'start' in data and first:
+            task = asyncio.create_task(send())
+            first = False
+
+        logger.info(f"Received data: {data}")
         if data == "exit":
-            await websocket.close()
+            _ = task.cancel() if task else None
             break
-        await websocket.send_text(f"Enter data: {data} with timestamp: {datetime.now()}")
+    
+    await websocket.close()
+        
